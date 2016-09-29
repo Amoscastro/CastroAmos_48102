ca5e40ee9faab9bddb14c8c836468edc1f4878d4
Amoscastro <amos.castro.rvp1@gmail.com>
2016-09-29 12:24:01 -0700

    Signed-off-by: Amoscastro <amos.castro.rvp1@gmail.com>

---
+++ Homework/Assignment 3/Gaddis_8thEd_Ch4_Prob/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: Amos Castro Jr.
+ * Created on September 29th, 2016 12:03
+ * Purpose:  To determine the Band of Radiation from the wave length
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    float waveLen;
+    //Input or initialize values Here
+    cout<<"Input wave length of radiation in Meters"<<endl;
+    cout<<"This Program Determines The Band of Radiation."<<endl;
+    cin>>waveLen;
+    //Process/Calculations Here
+    if(waveLen>1e-2f)      cout<<"Radio Band"<<endl;
+    else if(waveLen>1e-3f) cout<<"Micro wave Band"<<endl;
+    else if(waveLen>7e-7f) cout<<"Infra-Red Band"<<endl;
+    else if(waveLen>4e-7f) cout<<"Visible Band"<<endl;
+    else if(waveLen>1e-8f) cout<<"Ultra-Violent Band"<<endl;
+    else if(waveLen>1e-11f)cout<<"X-Ray Band"<<endl;
+    else                   cout<<"Gamma-Ray Band"<<endl;
+    
+    //Output Located Here
+
+
+    //Exit
+    return 0;
+}
